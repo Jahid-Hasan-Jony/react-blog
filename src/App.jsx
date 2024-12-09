b71e1a439a331ed4203bff1f46a9245b3b09809e
Jahid-Hasan-Jony <jonyhossain5588@gmail.com>
2024-12-09 18:51:50 +0600

    Excited to move forwards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold">
+        Hello world!, Welcome to mern stack development
+      </h1>
     </>
   );
 }
